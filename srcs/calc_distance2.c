0c3155950e9efbec4496e972c484d6f0ee8a79b8
Jano844 <jani.saenger@gmail.com>
2023-12-23 14:41:31 +0100

    calc dist

--- srcs/calc_distance2.c
+++ srcs/calc_distance2.c
@@ -6,23 +6,23 @@
 /*   By: jsanger <jsanger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 21:05:04 by jsanger           #+#    #+#             */
-/*   Updated: 2023/12/23 00:38:18 by jsanger          ###   ########.fr       */
+/*   Updated: 2023/12/23 14:41:16 by jsanger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	player_pos_x = 1;
-int	player_pos_y = 2;
+int	player_pos_x = 3;
+int	player_pos_y = 3;
 
 char map[7][7] =
 {
 		{'1', '1', '1', '1', '1', '1', '1'},
-		{'1', '0', '0', '0', '0', '0', '1'},
-		{'1', 'P', '0', '0', '0', '0', '1'},
-		{'1', '0', '0', '0', '0', '0', '1'},
-		{'1', '0', '0', '0', '0', '0', '1'},
-		{'1', '0', '0', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1', '1', '1'},
+		{'1', '1', '1', '0', '0', '1', '1'},
+		{'1', '1', '1', 'P', '1', '1', '1'},
+		{'1', '1', '1', '1', '1', '1', '1'},
+		{'1', '1', '1', '1', '1', '1', '1'},
 		{'1', '1', '1', '1', '1', '1', '1'}
 };
 
@@ -30,12 +30,15 @@ float	get_slope(float angle)
 {
 	float radianAngle;
 
-	if (angle == 0 || angle == 90 || angle == 180 || angle == 270 || angle == 360)
-		return (INT32_MAX);
+
 	while (angle > 360)
 		angle -= 360;
 	while (angle < 0)
 		angle += 360;
+	if (angle == 90)
+		return (INT32_MAX);
+	if (angle == 270)
+		return (INT32_MIN);
 	radianAngle = (angle) * PI / 180.0;
 	float dirx = cos(radianAngle);
 	float diry = sin(radianAngle);
@@ -45,23 +48,17 @@ float	get_slope(float angle)
 float	calc_sx_sy(float m, char dir)
 {
 	if (dir == 'y')
+	{
+		if (m == 0)
+			return(INT32_MAX);
 		m = 1/m;
+	}
 	return (sqrt(1 + pow(m, 2)));
 }
 
 int	check_wall_hit(char dir, int step_x, int step_y, float angle)
 {
-	// if (angle >= 0 && angle <= 180)
-	// 	step_y++;
-	// else
-	// 	step_y--;
-	// if (angle >= 90 && angle <= 270)
-	// 	step_x--;
-	// else
-	// 	step_x++;
-	// printf("%d  %d\n", player_pos_x - step_x, player_pos_y - step_y);
-	// printf("%c\n", map[player_pos_y - step_y][player_pos_x - step_x]);
-	if (map[player_pos_y - step_y][player_pos_x - step_x] == '1')
+	if (map[player_pos_y + step_y][player_pos_x + step_x] == '1')
 		return (1);
 	return (0);
 }
@@ -78,9 +75,9 @@ float	calc_distance(float m, float angle)
 	float		step_dir_x;
 
 	if (angle >= 0 && angle <= 180)
-		step_dir_y = 1;
-	else
 		step_dir_y = -1;
+	else
+		step_dir_y = 1;
 	if (angle >= 90 && angle <= 270)
 		step_dir_x = -1;
 	else
@@ -94,17 +91,23 @@ float	calc_distance(float m, float angle)
 		{
 			int	test = check_wall_hit('y', stepcount_x, stepcount_y, angle);
 			if (test == 1)
-				return (stepcount_x * calc_sx_sy(m, 'x') + stepcount_y * calc_sx_sy(m , 'y'));
+				return ((stepcount_y - step_dir_y) * calc_sx_sy(m , 'y'));
 			stepcount_y += step_dir_y;
 			dist_y += calc_sx_sy(m, 'y');
+			test = check_wall_hit('y', stepcount_x, stepcount_y, angle);
+			if (test == 1)
+				return ((stepcount_y - step_dir_y)* calc_sx_sy(m , 'y'));
 		}
 		while (dist_y > dist_x)
 		{
 			int	test = check_wall_hit('x', stepcount_x, stepcount_y, angle);
 			if (test == 1)
-				return (stepcount_x * calc_sx_sy(m, 'x') + stepcount_y * calc_sx_sy(m , 'y'));
+				return ((stepcount_x - step_dir_x) * calc_sx_sy(m, 'x'));
 			stepcount_x += step_dir_x;
 			dist_x += calc_sx_sy(m, 'x');
+			test = check_wall_hit('x', stepcount_x, stepcount_y, angle);
+			if (test == 1)
+				return ((stepcount_x - step_dir_x)* calc_sx_sy(m, 'x'));
 		}
 	}
 }
@@ -117,5 +120,7 @@ int	main()
 		m = -m;
 	printf("%f\n", m);
 	float dist = calc_distance(m, angle);
+	if (dist < 0)
+		dist *= -1;
 	printf("Distance: %f\n", dist);
 }
